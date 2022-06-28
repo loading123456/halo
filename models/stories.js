5c2535d08b77392b81571e42d48716fb3e6ee221
Vu_dinh_dung <cotenk7@gmail.com>
2022-06-28 16:02:28 +0700

    new

--- models/stories.js
+++ models/stories.js
@@ -4,10 +4,10 @@ const fs = require("fs")
 module.exports.stories = {}
 
 module.exports.load_stories = ()=>{
-    let json_files = fs.readdirSync("./")
+    let json_files = fs.readdirSync("./storage/jsons")
 
-    // for(let i=0; i<json_files.length; i++){
-    //     this.stories[json_files[i].replace(".json", '')] = fs.readFileSync(`../storage/jsons/${json_files[i]}`)
-    // }
-    console.log("Load stories: ",json_files)
+    for(let i=0; i<json_files.length; i++){
+        this.stories[json_files[i].replace(".json", '')] = fs.readFileSync(`../storage/jsons/${json_files[i]}`)
+    }
+    console.log("Load stories: ",Object.keys(this.stories))
 }
