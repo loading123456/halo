40ef66317057447b91e3cb5c4e5cf832fa8ee6a1
Vu_dinh_dung <cotenk7@gmail.com>
2022-06-28 15:59:46 +0700

    new

--- models/stories.js
+++ models/stories.js
@@ -4,7 +4,7 @@ const fs = require("fs")
 module.exports.stories = {}
 
 module.exports.load_stories = ()=>{
-    let json_files = fs.readdirSync("../storage/jsons")
+    let json_files = fs.readdirSync("")
 
     // for(let i=0; i<json_files.length; i++){
     //     this.stories[json_files[i].replace(".json", '')] = fs.readFileSync(`../storage/jsons/${json_files[i]}`)
