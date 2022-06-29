6069d186b82bb198a3215807d42f3f218ff3127e
Vu_dinh_dung <cotenk7@gmail.com>
2022-06-29 15:43:32 +0700

    new

--- controllers/actions.js
+++ controllers/actions.js
@@ -55,7 +55,7 @@ module.exports.extract_imgs = (req, res)=>{
   let action = spawn("python3", ["python3/extract.py",story_name])
 
     action.on("close",()=>{
-      fs.copyFileSync(`/storage/emulated/nexus/halo/'${story_name}.zip`, `/storage/emulated/0/halo/storage/untran_imgs/${stories.story_name}.zip`)
+      fs.copyFileSync(`/storage/emulated/nexus/halo/'${story_name}.zip`, `/storage/emulated/0/halo/storage/untran_imgs/${story_name}.zip`)
       fs.unlinkSync(`/storage/emulated/nexus/halo/'${story_name}.zip`)
  
       stories.stories[story_name].stage =  "Extracting"
